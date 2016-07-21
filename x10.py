81ed187c89cc40d8fac13565b309f2b6dd938d57
clach04 <Chris.Clark@actian.com>
2016-07-21 16:57:59 -0700

    Require later version of x10_any for Python 3.4
    and also for real Mochad servers

--- x10.py
+++ x10.py
@@ -5,7 +5,7 @@ import logging
 from homeassistant.helpers.entity import ToggleEntity
 from homeassistant.const import DEVICE_DEFAULT_NAME, CONF_HOST, CONF_PORT, CONF_FILENAME
 
-REQUIREMENTS = ['x10_any>=0.0.5']
+REQUIREMENTS = ['x10_any>=0.0.6']
 
 _LOGGER = logging.getLogger(__name__)
 
